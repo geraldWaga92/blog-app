cdc698d513ce455de73ed6a24927b089eea3d3f8
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-02-23 10:53:26 -0800

    Initial commit

---
+++ client/src/Header.js
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { UserContext } from "./UserContext";
+
+export default function Header() {
+  const { userInfo, setUserInfo } = useContext(UserContext);
+  useEffect(() => {
+    fetch('http://localhost:5000/profile', {
+      credentials: 'include',
+    }).then(response => {
+      response.json().then(userInfo => {
+        setUserInfo(userInfo)
+      })
+    });
+  }, []);
+
+  function logout() {
+    fetch('http://localhost:5000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    });
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
+
+  return (
+    <header>
+      <Link to="/" className="logo">Sougene's Daily Blog</Link>
+      <nav>
+        {username && (
+          <>
+            <Link to="/create" className="create" >Create new post</Link>
+            <a onClick={logout} className="logout" >Logout ({username})</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
